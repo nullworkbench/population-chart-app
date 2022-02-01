4fb7022a36eb50543505cfd31120b40863530f32
nullworkbench <nullworkbench@gmail.com>
2022-02-01 16:20:06 +0900

    選択している都道府県を配列で管理するように

--- src/components/SelectPrefectures.tsx
+++ src/components/SelectPrefectures.tsx
@@ -1,11 +1,35 @@
 // 都道府県を選択するチェックボックス
+
+import { ChangeEvent } from "react";
 import styled from "styled-components";
 
 import { usePrefectures } from "@/libs/ResasApi";
 
-const SelectPrefectures: React.FC = () => {
+type Props = {
+  // 親のuseStateを呼び出す
+  selectedPrefs: number[];
+  setSelectedPrefs: Function;
+};
+
+const SelectPrefectures: React.FC<Props> = (prop) => {
   // 都道府県一覧を取得
   const { prefectures, isLoading, isError } = usePrefectures();
+
+  // チェックを切り替えたときに選択一覧の配列を更新する
+  function handleCheckboxChange(
+    e: ChangeEvent<HTMLInputElement>,
+    prefCode: number
+  ) {
+    if (e.target.value) {
+      // 選択したとき
+      // 引数のprefCodeと一致しないもののみを返す（一致するものを削除する）
+      const _new = prop.selectedPrefs.filter((pref, idx) => pref != prefCode);
+      _new.push(prefCode);
+      prop.setSelectedPrefs([..._new]);
+    } else {
+      // 選択を解除したとき
+    }
+  }
 
   return (
     <div>
@@ -15,11 +39,18 @@ const SelectPrefectures: React.FC = () => {
         <Wrapper>
           {prefectures()
             ? prefectures()!.map((prefecture, prefIdx) => (
-                <CheckBox
-                  key={prefIdx}
-                  prefCode={prefecture.prefCode}
-                  prefName={prefecture.prefName}
-                />
+                <CheckBoxWrap key={prefIdx}>
+                  <input
+                    type="checkbox"
+                    id={"pref" + prefecture.prefCode}
+                    onChange={(e) =>
+                      handleCheckboxChange(e, prefecture.prefCode)
+                    }
+                  />
+                  <label htmlFor={"pref" + prefecture.prefCode}>
+                    {prefecture.prefName}
+                  </label>
+                </CheckBoxWrap>
               ))
             : "Error"}
         </Wrapper>
@@ -33,17 +64,9 @@ const Wrapper = styled.div`
   flex-wrap: wrap;
 `;
 
-const CheckBox: React.FC<{ prefCode: number; prefName: string }> = (prop) => {
-  const Div = styled.div`
-    width: 6rem;
-    padding: 1rem 0.5rem;
-  `;
-  return (
-    <Div>
-      <input type="checkbox" id={"pref" + prop.prefCode} />
-      <label htmlFor={"pref" + prop.prefCode}>{prop.prefName}</label>
-    </Div>
-  );
-};
+const CheckBoxWrap = styled.div`
+  width: 6rem;
+  padding: 1rem 0.5rem;
+`;
 
 export default SelectPrefectures;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,25 @@ import type { NextPage } from "next";
 import styles from "@/styles/Home.module.scss";
 
 import SelectPrefectures from "@/components/SelectPrefectures";
+import { useState } from "react";
 
 const Home: NextPage = () => {
+  // 選択中の都道府県のPrefCode
+  const [selectedPrefs, setSelectedPrefs] = useState<number[]>([]);
+
   return (
     <>
       {/* 都道府県一覧 */}
-      <SelectPrefectures />
+      <SelectPrefectures
+        selectedPrefs={selectedPrefs}
+        setSelectedPrefs={setSelectedPrefs}
+      />
+      <div>
+        <p>選択中の都道府県</p>
+        {selectedPrefs.map((pref, prefIdx) => (
+          <span key={prefIdx}>{pref} </span>
+        ))}
+      </div>
     </>
   );
 };
