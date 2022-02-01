fabeace9a263dc62c94fbadbad2632a68f5358bb
nullworkbench <nullworkbench@gmail.com>
2022-02-01 14:25:13 +0900

    都道府県を表示する機能をSelectPrefecturesに移植

--- src/components/SelectPrefectures.tsx
+++ src/components/SelectPrefectures.tsx
@@ -1,7 +1,26 @@
 // 都道府県を選択するチェックボックス
 
+import { usePrefectures } from "@/libs/ResasApi";
+
 const SelectPrefectures: React.FC = () => {
-  return <div></div>;
+  // 都道府県一覧を取得
+  const { prefectures, isLoading, isError } = usePrefectures();
+
+  return (
+    <div>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          {prefectures()
+            ? prefectures()!.map((prefecture, prefIdx) => (
+                <div key={prefIdx}>{prefecture["prefName"]}</div>
+              ))
+            : "Error"}
+        </>
+      )}
+    </div>
+  );
 };
 
 export default SelectPrefectures;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,13 @@
 import type { NextPage } from "next";
 import styles from "@/styles/Home.module.scss";
 
-import { usePrefectures } from "@/libs/ResasApi";
+import SelectPrefectures from "@/components/SelectPrefectures";
 
 const Home: NextPage = () => {
-  // 都道府県一覧を取得
-  const { prefectures, isLoading, isError } = usePrefectures();
-
   return (
     <>
       {/* 都道府県一覧 */}
-      <div>
-        {isLoading ? (
-          <p>Loading...</p>
-        ) : (
-          <>
-            {prefectures()
-              ? prefectures()!.map((prefecture, prefIdx) => (
-                  <div key={prefIdx}>{prefecture["prefName"]}</div>
-                ))
-              : "Error"}
-          </>
-        )}
-      </div>
+      <SelectPrefectures />
     </>
   );
 };
