e7171027264ca7046437339d41e65a3a1dacdda9
nullworkbench <nullworkbench@gmail.com>
2022-02-02 18:44:20 +0900

    選択中の都道府県の型変更に対応

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -3,16 +3,16 @@ import Highcharts from "highcharts";
 import HighchartsReact from "highcharts-react-official";
 
 // 総人口を取得する関数
-import { Population, usePopulation, getPopulation } from "@/libs/ResasApi";
+import { Prefecture, Population, getPopulation } from "@/libs/ResasApi";
 import { useState } from "react";
 
 type Props = {
-  selectedPrefs: number[];
+  selectedPrefs: Prefecture[];
 };
 
 const Chart: React.FC<Props> = (prop) => {
   // グラフに表示する都道府県のprefCode
-  const prefCodes = prop.selectedPrefs;
+  const prefs = prop.selectedPrefs;
 
   // グラフの設定
   const [chartOptions, setChartOptions] = useState({
@@ -26,19 +26,19 @@ const Chart: React.FC<Props> = (prop) => {
     yAxis: { title: { text: "総人口" } },
     // グラフ
     series: [
-      { type: "line", name: "graph1", prefCode: 0, data: [1, 2, 3] },
-      { type: "line", name: "graph2", prefCode: 0, data: [3, 2, 1] },
+      { type: "line", name: "graph1", data: [1, 2, 3] },
+      { type: "line", name: "graph2", data: [3, 2, 1] },
     ],
   });
 
   // prefCodeから総人口を取得
   async function getPopulations() {
-    for (const prefCode of prefCodes) {
+    for (const pref of prefs) {
       // すでにグラフに追加されているものはスキップ
-      if (chartOptions.series.find((series) => series.prefCode == prefCode))
+      if (chartOptions.series.find((series) => series.name == pref.prefName))
         continue;
       // 情報を取得してグラフへ描画
-      const population = await getPopulation(prefCode);
+      const population = await getPopulation(pref.prefCode);
       if (population) {
         updateChart(population);
       }
@@ -53,7 +53,6 @@ const Chart: React.FC<Props> = (prop) => {
         {
           type: "line",
           name: `${newData.prefCode}`,
-          prefCode: newData.prefCode,
           data: newData.data.map((d) => d.value),
         },
       ],
@@ -64,8 +63,8 @@ const Chart: React.FC<Props> = (prop) => {
     <>
       <button onClick={() => getPopulations()}>updateChart</button>
       <div>
-        {prefCodes.map((code, idx) => (
-          <span key={idx}>{code} </span>
+        {prefs.map((pref, idx) => (
+          <span key={idx}>{pref.prefCode} </span>
         ))}
       </div>
       <HighchartsReact highcharts={Highcharts} options={chartOptions} />
