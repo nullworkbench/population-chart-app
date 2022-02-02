c91fd690288861f96cfde60bd72fd14712c6046f
nullworkbench <nullworkbench@gmail.com>
2022-02-02 12:34:21 +0900

    usePopulationが正常に動作するかのテストを実装

--- __test__/usePopulation.test.tsx
+++ __test__/usePopulation.test.tsx
@@ -1,6 +1,7 @@
 // テスト用ライブラリ
 import { cleanup, waitFor } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
+import { renderHook } from "@testing-library/react-hooks";
 import { rest } from "msw";
 import { setupServer } from "msw/node";
 
@@ -37,5 +38,19 @@ afterAll(() => {
 
 // テスト内容
 describe("usePopulationのテスト", () => {
-  test("APIから正常に取得できるか確認", async () => {});
+  test("APIから正常に取得できるか確認", async () => {
+    // usePopulation実行
+    const { result } = renderHook(() => usePopulation(1));
+
+    // isLoadingがfalseになるまで待つ
+    await waitFor(() => {
+      const isLoading = result.current.isLoading;
+      expect(isLoading).toBe(false);
+    });
+
+    // APIのレスポンスから総人口を切り出したもの
+    const expectedResponse = population.result.data[0].data;
+    // データがきちんと整形されて取得されているか確認
+    expect(result.current.population()).toStrictEqual(expectedResponse);
+  });
 });
