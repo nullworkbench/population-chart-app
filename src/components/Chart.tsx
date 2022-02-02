9dbbb89d3a03be5baa10f608f12e4b077013a8bd
nullworkbench <nullworkbench@gmail.com>
2022-02-02 15:07:20 +0900

    テストデータでグラフを描画

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -3,7 +3,27 @@ import Highcharts from "highcharts";
 import HighchartsReact from "highcharts-react-official";
 
 const Chart: React.FC = () => {
-  return <div></div>;
+  const chartOptions = {
+    //グラフタイトル
+    title: { text: "都道府県別の総人口推移" },
+    subtitle: { text: "出典：内閣府 地方創生推進室 地域経済分析システム" },
+    // X軸のラベル
+    xAxis: {
+      categories: ["A", "B", "C"],
+    },
+    yAxis: { title: { text: "総人口" } },
+    // グラフ
+    series: [
+      { type: "line", name: "graph1", data: [1, 2, 3] },
+      { type: "line", name: "graph2", data: [3, 2, 1] },
+    ],
+  };
+
+  return (
+    <div>
+      <HighchartsReact highcharts={Highcharts} options={chartOptions} />
+    </div>
+  );
 };
 
 export default Chart;
