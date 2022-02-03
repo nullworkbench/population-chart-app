6d8db3cf4f9fe0d08a0ce22cff72db42c6f76924
nullworkbench <nullworkbench@gmail.com>
2022-02-03 16:55:11 +0900

    RESAS-API独自の400エラーを検出できているか確認するテストを実装

--- __test__/isRESASError.test.ts
+++ __test__/isRESASError.test.ts
@@ -5,6 +5,9 @@ import "@testing-library/jest-dom/extend-expect";
 // テスト対象
 import { isRESASError } from "@/libs/ResasApi";
 
+// モックデータ
+import data from "./apiMockData/resasErrorResponses.json";
+
 afterEach(() => {
   cleanup();
 });
@@ -12,5 +15,11 @@ afterEach(() => {
 // テスト内容
 describe("RESAS-APIのエラーが検出できるか", () => {
   // テスト１
-  test("", async () => {});
+  test("400 Bad Request", async () => {
+    const expectedError = {
+      statusCode: 400,
+      errorMessage: "Some Error Occured.",
+    };
+    expect(isRESASError(data[400])).toStrictEqual(expectedError);
+  });
 });
