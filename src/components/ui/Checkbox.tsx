6cc408aa87c55394f27407b82f1fe0f709a7b23c
nullworkbench <nullworkbench@gmail.com>
2022-02-03 21:28:27 +0900

    Checkboxのサイズを調整

--- src/components/ui/Checkbox.tsx
+++ src/components/ui/Checkbox.tsx
@@ -21,17 +21,17 @@ const Checkbox: React.FC<Props> = (prop) => {
 export default Checkbox;
 
 const Label = styled.label`
-  --w: 7rem;
+  --w: 6rem;
   --color: #26c485;
   display: block;
   text-align: center;
 
   & > span {
     display: block;
-    width: var(--w);
-    padding: 0.5rem 1rem;
+    // width: var(--w);
+    padding: 0.2rem 1rem;
     border: 0.1rem solid #26c485;
-    border-radius: calc(var(--w) / 2);
+    border-radius: 10rem;
     color: var(--color);
   }
 
