7fb50f2dbaaf55372007534e389eb02083bcaed8
nullworkbench <nullworkbench@gmail.com>
2022-02-04 08:38:36 +0900

    ResasApiを叩くためのaxiosインスタンスを作成

--- src/pages/api/prefectures.ts
+++ src/pages/api/prefectures.ts
@@ -1,6 +1,19 @@
+import axios from "axios";
 import { NextApiRequest, NextApiResponse } from "next";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+import { Prefecture } from "@/libs/ResasApi";
+
+// resasApiを使うための基本設定を行なったaxiosインスタンス
+const resasApi = axios.create({
+  baseURL: "https://opendata.resas-portal.go.jp/api/v1",
+  headers: { "x-api-key": process.env.NEXT_PUBLIC_RESAS_API_KEY! },
+});
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Prefecture[] | string>
+) {
   // ResasApiから都道府県一覧を取得して返す
-  res.status(200).json({ name: "John Doe" });
+  resasApi.get("/prefectures").then((axiosRes) => {});
+  res.status(200).json("John");
 }
