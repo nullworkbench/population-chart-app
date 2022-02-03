de94c8bda54a9d129865ce56d35857343a87f510
nullworkbench <nullworkbench@gmail.com>
2022-02-03 14:24:35 +0900

    グラフの操作をコンポーネント管理からページでの管理に変更

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -2,72 +2,14 @@
 import Highcharts from "highcharts";
 import HighchartsReact from "highcharts-react-official";
 
-// 総人口を取得する関数
-import { Prefecture, Population, getPopulation } from "@/libs/ResasApi";
-import { useState } from "react";
-
 type Props = {
-  selectedPrefs: Prefecture[];
+  chartOptions: Highcharts.Options;
 };
 
 const Chart: React.FC<Props> = (prop) => {
-  // グラフに表示する都道府県のprefCode
-  const prefs = prop.selectedPrefs;
-
-  // グラフの設定
-  const [chartOptions, setChartOptions] = useState({
-    //グラフタイトル
-    title: { text: "都道府県別の総人口推移" },
-    subtitle: { text: "出典：内閣府 地方創生推進室 地域経済分析システム" },
-    // X軸のラベル
-    xAxis: {
-      categories: ["A", "B", "C"],
-    },
-    yAxis: { title: { text: "総人口" } },
-    // グラフ
-    series: [
-      { type: "line", name: "graph1", data: [1, 2, 3] },
-      { type: "line", name: "graph2", data: [3, 2, 1] },
-    ],
-  });
-
-  // prefCodeから総人口を取得
-  async function getPopulations() {
-    for (const pref of prefs) {
-      // すでにグラフに追加されているものはスキップ
-      if (chartOptions.series.find((series) => series.name == pref.prefName))
-        continue;
-      // 情報を取得してグラフへ描画
-      const population = await getPopulation(pref.prefCode);
-      if (population) {
-        updateChart(population, pref.prefName);
-      }
-    }
-  }
-
-  function updateChart(newData: Population, prefName: string) {
-    setChartOptions({
-      ...chartOptions,
-      series: [
-        ...chartOptions.series,
-        {
-          type: "line",
-          name: prefName,
-          data: newData.data.map((d) => d.value),
-        },
-      ],
-    });
-  }
-
   return (
     <>
-      <button onClick={() => getPopulations()}>updateChart</button>
-      <div>
-        {prefs.map((pref, idx) => (
-          <span key={idx}>{pref.prefCode} </span>
-        ))}
-      </div>
-      <HighchartsReact highcharts={Highcharts} options={chartOptions} />
+      <HighchartsReact highcharts={Highcharts} options={prop.chartOptions} />
     </>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,30 @@
 import type { NextPage } from "next";
 import { useState } from "react";
 
-import { Prefecture } from "@/libs/ResasApi";
+import { Prefecture, Population, getPopulation } from "@/libs/ResasApi";
 import SelectPrefectures from "@/components/SelectPrefectures";
 import Chart from "@/components/Chart";
 
 const Home: NextPage = () => {
   // 選択中の都道府県のPrefCode
   const [selectedPrefs, setSelectedPrefs] = useState<Prefecture[]>([]);
+
+  // グラフの設定
+  const [chartOptions, setChartOptions] = useState<Highcharts.Options>({
+    //グラフタイトル
+    title: { text: "都道府県別の総人口推移" },
+    subtitle: { text: "出典：内閣府 地方創生推進室 地域経済分析システム" },
+    // X軸のラベル
+    xAxis: {
+      categories: ["A", "B", "C"],
+    },
+    yAxis: { title: { text: "総人口" } },
+    // グラフ
+    series: [
+      { type: "line", name: "graph1", data: [1, 2, 3] },
+      { type: "line", name: "graph2", data: [3, 2, 1] },
+    ],
+  });
 
   // チェックを切り替えたとき
   function handleCheckboxChange(checked: boolean, pref: Prefecture) {
@@ -21,6 +38,12 @@ const Home: NextPage = () => {
     setSelectedPrefs(_new);
   }
 
+  // グラフの更新
+  function updateChart() {}
+
+  // 総人口を取得する関数
+  function getPopulation(prefCode: number) {}
+
   return (
     <>
       {/* 都道府県一覧 */}
@@ -29,7 +52,7 @@ const Home: NextPage = () => {
         handleCheckboxChange={handleCheckboxChange}
       />
       {/* グラフ */}
-      <Chart selectedPrefs={selectedPrefs} />
+      <Chart chartOptions={chartOptions} />
     </>
   );
 };
