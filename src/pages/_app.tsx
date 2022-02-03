0094e173072b42c325623b44aa286edb189a4c33
nullworkbench <nullworkbench@gmail.com>
2022-02-03 20:20:15 +0900

    ページの最大幅を設定

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,22 @@
 import type { AppProps } from "next/app";
+import styled from "styled-components";
 import "@/styles/globals.scss";
 
 import Title from "@/components/ui/Title";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <Wrapper>
       <Title />
       <Component {...pageProps} />
-    </>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.div`
+  width: 90vw;
+  max-width: 1000px;
+  margin: 0 auto;
+`;
 
 export default MyApp;
