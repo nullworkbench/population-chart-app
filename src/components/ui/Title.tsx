9e1a8724fe826c755b492953635bb8066696da30
nullworkbench <nullworkbench@gmail.com>
2022-02-03 20:38:28 +0900

    Titleコンポーネントの下部に余白を追加

--- src/components/ui/Title.tsx
+++ src/components/ui/Title.tsx
@@ -1,9 +1,15 @@
+import styled from "styled-components";
+
 const Title: React.FC = () => {
   return (
     <div>
-      <h1>Population Chart</h1>
+      <H1>Population Chart</H1>
     </div>
   );
 };
+
+const H1 = styled.h1`
+  margin-bottom: 2rem;
+`;
 
 export default Title;
