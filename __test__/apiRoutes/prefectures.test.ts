b106961318d74c08c65ff46a2747f454dcbfef73
nullworkbench <nullworkbench@gmail.com>
2022-02-04 10:36:29 +0900

    NextのAPIルートをテストできるように

--- __test__/apiRoutes/prefectures.test.ts
+++ __test__/apiRoutes/prefectures.test.ts
@@ -1,8 +1,10 @@
 // テスト用ライブラリ
-import { cleanup, screen } from "@testing-library/react";
+import { cleanup, screen, waitFor } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import { rest } from "msw";
 import { setupServer } from "msw/node";
+import httpMocks from "node-mocks-http";
+import { NextApiRequest, NextApiResponse } from "next";
 
 // テスト対象
 import prefecutresApiRoute from "@/pages/api/prefectures";
@@ -11,13 +13,9 @@ import prefectures from "../apiMockData/prefectures.json";
 // APIのモックサーバーを立てる
 const apiURL = "https://opendata.resas-portal.go.jp/api/v1/prefectures";
 const handlers = [
-  rest.get(
-    // テスト中の以下のリクエストはこのモックサーバーに送られる
-    apiURL,
-    (req, res, ctx) => {
-      return res(ctx.status(200), ctx.json(prefectures));
-    }
-  ),
+  rest.get(apiURL, (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(prefectures));
+  }),
 ];
 const server = setupServer(...handlers);
 beforeAll(() => {
@@ -33,5 +31,10 @@ afterAll(() => {
 
 // テスト内容
 describe("/api/prefecturesでResasApiを叩いて都道府県一覧を返す内部API", () => {
-  test("正しく取得できる場合", async () => {});
+  test("正しく取得できる場合", async () => {
+    const mockReq = httpMocks.createRequest<NextApiRequest>();
+    const mockRes = httpMocks.createResponse<NextApiResponse>();
+    prefecutresApiRoute(mockReq, mockRes);
+    expect(mockRes._getJSONData()).toStrictEqual("Jhn");
+  });
 });
