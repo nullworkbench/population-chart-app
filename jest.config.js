8dfa2aa67791e40e812e0045cac151c0bee190b2
nullworkbench <nullworkbench@gmail.com>
2022-02-04 16:00:22 +0900

    テストのセットアップ用のファイルをテスト対象から除外

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,11 @@ module.exports = {
   // セットアップで実行する
   globalSetup: "<rootDir>/__tests__/setupEnv.ts",
   // テスト対象から除外
-  testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
+  testPathIgnorePatterns: [
+    "<rootDir>/.next/",
+    "<rootDir>/node_modules/",
+    "<rootDir>/__tests__/setupEnv.ts",
+  ],
   // モジュールのパス指定
   moduleNameMapper: {
     "\\.(scss)$": "<rootDir>/node_modules/jest-css-modules",
