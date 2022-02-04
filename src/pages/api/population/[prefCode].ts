0ba84c5a663e72ce0c827d940a9a89931db98c0e
nullworkbench <nullworkbench@gmail.com>
2022-02-04 14:06:03 +0900

    /api/prefectures.tsをもとに、人口情報を取得するメソッドを記述

--- src/pages/api/population/[prefCode].ts
+++ src/pages/api/population/[prefCode].ts
@@ -1,5 +1,8 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
+import { resasApi, isRESASError, Population } from "@/libs/ResasApi";
+import { AxiosError } from "axios";
+
 // 拡張してprefCodeを型補完（queryはstring型のみ）
 interface ExNextApiRequest extends NextApiRequest {
   query: {
@@ -7,10 +10,31 @@ interface ExNextApiRequest extends NextApiRequest {
   };
 }
 
-function useHandler(req: ExNextApiRequest, res: NextApiResponse) {
-  const {
-    query: { prefCode },
-  } = req;
+async function useHandler(req: ExNextApiRequest, res: NextApiResponse) {
+  const prefCode = Number(req.query.prefCode);
+
+  // ResasApiから人口情報を取得して返す
+  await resasApi
+    .get(`/population/composition/perYear?prefCode=${prefCode}&cityCode=-`)
+    .then((axiosRes) => {
+      // ResasApi独自のエラーをチェック
+      const resasError = isRESASError(axiosRes.data);
+      if (resasError) {
+        // エラーがあればステータスコードとエラーメッセージを書き込む
+        console.log(`${resasError.statusCode}: ${resasError.errorMessage}`);
+        res.writeHead(resasError.statusCode, resasError.errorMessage);
+      } else {
+        // 正常に完了
+        const population: Population = {
+          prefCode,
+          data: axiosRes.data["result"]["data"][0]["data"],
+        };
+        res.status(200).json(population);
+      }
+    })
+    .catch((error: AxiosError) => {
+      res.writeHead(Number(error.code ?? 500), error.message);
+    });
 }
 
 export default useHandler;
