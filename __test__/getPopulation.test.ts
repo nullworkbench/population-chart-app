ac449a995949b9f01946002149f8091697a80a5c
nullworkbench <nullworkbench@gmail.com>
2022-02-04 14:51:09 +0900

    getPopulation関数の変更に伴い、テストを更新

--- __test__/getPopulation.test.ts
+++ __test__/getPopulation.test.ts
@@ -5,7 +5,7 @@ import { rest } from "msw";
 import { setupServer } from "msw/node";
 
 // テスト対象
-import { getPopulation } from "@/libs/ResasApi";
+import { getPopulation, Population } from "@/libs/ResasApi";
 
 // テスト用データ
 import population from "./apiMockData/population.json";
@@ -15,15 +15,18 @@ const prefCode = 1;
 import resasResponses from "./apiMockData/resasErrorResponses.json";
 
 // APIのモックサーバーを立てる
-const apiURL =
-  "https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear";
+const apiURL = `/api/population/${prefCode}`;
 const handlers = [
   rest.get(
     // テスト中の以下のリクエストはこのモックサーバーに送られる
     apiURL,
     // ダミーデータ
     (req, res, ctx) => {
-      return res(ctx.status(200), ctx.json(population));
+      const result: Population = {
+        prefCode,
+        data: population.result.data[0].data,
+      };
+      return res(ctx.status(200), ctx.json(result));
     }
   ),
 ];
@@ -46,8 +49,8 @@ describe("getPopulationのテスト", () => {
     const res = await getPopulation(prefCode);
 
     // APIのレスポンスから総人口を切り出したもの
-    const expectedResponse = {
-      prefCode: prefCode,
+    const expectedResponse: Population = {
+      prefCode,
       data: population.result.data[0].data,
     };
 
@@ -59,19 +62,6 @@ describe("getPopulationのテスト", () => {
     server.use(
       rest.get(apiURL, (req, res, ctx) => {
         return res(ctx.status(400));
-      })
-    );
-    // getPopulation実行
-    const res = await getPopulation(prefCode);
-
-    expect(res).toBeUndefined();
-  });
-
-  test("ResasApi独自のエラーがあった場合にvoidを返すことを確認", async () => {
-    // エラーを起こすためにモックサーバーを上書き
-    server.use(
-      rest.get(apiURL, (req, res, ctx) => {
-        return res(ctx.status(200), ctx.json(resasResponses["403"]));
       })
     );
     // getPopulation実行
