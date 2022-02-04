689fe00af7958e2bf37ec9da497b11c12ae77f91
nullworkbench <nullworkbench@gmail.com>
2022-02-04 12:47:30 +0900

    都道府県一覧を取得するAPIルートで、通信時にエラーがあった場合に検知できるように

--- src/pages/api/prefectures.ts
+++ src/pages/api/prefectures.ts
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import { NextApiRequest, NextApiResponse } from "next";
 
 import { Prefecture, isRESASError } from "@/libs/ResasApi";
@@ -14,15 +14,21 @@ export default async function handler(
   res: NextApiResponse<Prefecture[] | string>
 ) {
   // ResasApiから都道府県一覧を取得して返す
-  await resasApi.get("/prefectures").then((axiosRes) => {
-    // ResasApi独自のエラーをチェック
-    const resasError = isRESASError(axiosRes.data);
-    if (resasError) {
-      // エラーがあればステータスコードとエラーメッセージを書き込む
-      console.log(`${resasError.statusCode}: ${resasError.errorMessage}`);
-      res.writeHead(resasError.statusCode, resasError.errorMessage);
-    } else {
-      res.status(200).json(axiosRes.data["result"] as Prefecture[]);
-    }
-  });
+  await resasApi
+    .get("/prefectures")
+    .then((axiosRes) => {
+      // ResasApi独自のエラーをチェック
+      const resasError = isRESASError(axiosRes.data);
+      if (resasError) {
+        // エラーがあればステータスコードとエラーメッセージを書き込む
+        console.log(`${resasError.statusCode}: ${resasError.errorMessage}`);
+        res.writeHead(resasError.statusCode, resasError.errorMessage);
+      } else {
+        // 正常に完了
+        res.status(200).json(axiosRes.data["result"] as Prefecture[]);
+      }
+    })
+    .catch((error: AxiosError) => {
+      res.writeHead(Number(error.code ?? 500), error.message);
+    });
 }
