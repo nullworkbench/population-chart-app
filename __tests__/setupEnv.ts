cf28db64bda0cfc354deb6f3fb32233e34987c72
nullworkbench <nullworkbench@gmail.com>
2022-02-05 15:13:31 +0900

    テスト中はRESAS-APIのモックデータを使用するため、環境変数をセットするコードを削除

--- __tests__/setupEnv.ts
+++
@@ -1,8 +0,0 @@
-import { loadEnvConfig } from "@next/env";
-// 環境変数を扱うための設定
-const setupEnv = async () => {
-  const projectDir = process.cwd();
-  loadEnvConfig(projectDir);
-};
-
-export default setupEnv;
